43230dd3ade30a9ba72ac1fe7aa5b61c83f1e7cf
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-07-28 21:19:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Calculator/calculator.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static void utility(int a, int b, int opr) {
+        // code here
+        switch(opr){
+            case 1:
+                System.out.print(a+b);
+                break;
+            case 2:
+                System.out.print(a-b);
+                break;
+            case 3:
+                System.out.print(a*b);
+                break;
+            default:
+                System.out.print("Invalid Input");
+                
+        }
+    }
+}
