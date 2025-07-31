95b3a613f311e1b77a2e8b11e4eaa89dd20e8242
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-07-31 11:02:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LCM/lcm.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+    public static int LCM(int a, int b) {
+
+        // write your code here
+        int ans = a>b?a:b;
+        while(true){
+            if(ans%a==0 && ans%b==0){
+                break;
+            }
+            ans++;
+        }
+        return ans;
+        // return LCM of a and b
+    }
+}
