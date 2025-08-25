3b83572d86cde5ea35f67185f0bbdf9f6699d546
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-08-25 20:53:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Day before N days/day-before-n-days.java
+++ Difficulty: Easy/Day before N days/day-before-n-days.java
@@ -1,6 +1,8 @@
+// User function Template for Java
 class Solution {
-    public int dayBefore(int d, int n) {
-        // code here
-        return ((d-n)%7+7)%7;
+    public static int nthDay(int d, int n) {
+        // write your code here
+       return ((d-n)%7+7)%7;
+             
     }
 }
