9fae5a7da399625f90c267e44725855bd34b7587
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-09-05 12:09:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.java
+++ Difficulty: Easy/Reverse a Doubly Linked List/reverse-a-doubly-linked-list.java
@@ -14,15 +14,31 @@ class Node {
 class Solution {
     public Node reverse(Node head) {
         // code here
-        if(head==null || head.next==null) return head;
-        Node curr = head;
-        Node temp=null;
-        while(curr!=null){
-            temp=curr.prev;
-            curr.prev=curr.next;
-            curr.next =temp;
-            curr=curr.prev;
+        // if(head==null || head.next==null) return head;
+        // Node curr = head;
+        // Node temp=null;
+        // while(curr!=null){
+        //     temp=curr.prev;
+        //     curr.prev=curr.next;
+        //     curr.next =temp;
+        //     curr=curr.prev;
+        // }
+        // return temp.prev;
+        
+        //approch 2
+        
+        Stack<Integer> stack = new Stack<>();
+        Node temp = head;
+        while(temp!=null){
+            stack.push(temp.data);
+            temp=temp.next;
         }
-        return temp.prev;
+        temp=head;
+        while(temp!=null){
+            temp.data=stack.peek();
+            stack.pop();
+            temp=temp.next;
+        }
+        return head;
     }
 }
