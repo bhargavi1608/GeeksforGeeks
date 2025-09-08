15e1f07cab7cf8a42b7746a835e19814b9953d5e
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-09-08 11:31:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.java
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.java
@@ -1,118 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-
-class Gfg {
-
-    static Node buildTree(String str) {
-
-        if (str.length() == 0 || str.charAt(0) == 'N') {
-            return null;
-        }
-
-        String ip[] = str.split(" ");
-        // Create the root of the tree
-        Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-
-        Queue<Node> queue = new LinkedList<>();
-
-        queue.add(root);
-        // Starting from the second element
-
-        int i = 1;
-        while (queue.size() > 0 && i < ip.length) {
-
-            // Get and remove the front of the queue
-            Node currNode = queue.peek();
-            queue.remove();
-
-            // Get the current node's value from the string
-            String currVal = ip[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
-                queue.add(currNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= ip.length) break;
-
-            currVal = ip[i];
-
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                queue.add(currNode.right);
-            }
-            i++;
-        }
-
-        return root;
-    }
-
-    static void printInorder(Node root) {
-        if (root == null) return;
-
-        printInorder(root.left);
-        System.out.print(root.data + " ");
-
-        printInorder(root.right);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String s = br.readLine();
-            Node root = buildTree(s);
-            int key = Integer.parseInt(br.readLine());
-
-            Solution sol = new Solution();
-            ArrayList<Node> result = sol.findPreSuc(root, key);
-            Node pre = result.get(0);
-            Node suc = result.get(1);
-
-            if (pre != null)
-                System.out.print(pre.data + " ");
-            else
-                System.out.print("-1 ");
-
-            if (suc != null)
-                System.out.println(suc.data);
-            else
-                System.out.println("-1");
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 /* BST Node
 class Node {
     int data;
@@ -126,36 +11,40 @@ class Node {
 class Solution {
     public ArrayList<Node> findPreSuc(Node root, int key) {
         // code here
-         ArrayList<Node> list = new ArrayList<>();
-        list.add(new Node(Integer.MIN_VALUE)); 
-        list.add(new Node(Integer.MAX_VALUE));
-
-        // ArrayDeque is faster than others in single threaded programs 
-
-        Deque<Node> q = new ArrayDeque<>();
-
-        q.offer(root);
-        
-        while(!q.isEmpty()){
-            Node node = q.poll(); // why not q.remove()??? cause "poll" doesn't give NPE error but "remove" gives, so we don't have to check for NPE error
-            if(node.data < key){
-                if(list.get(0).data < node.data){
-                    list.set(0, node);
-                }
+        List<Node> list = new ArrayList<>();
+      
+        inorder(root,list);
+        Node pre=null, succ=null;
+        boolean flag = false;
+        for(int i=0;i<list.size();i++){
+            if(list.get(i).data>=key){
+               if(i>0) pre = list.get(i-1);
+               if(list.get(i).data==key && i+1<list.size()){
+                   succ = list.get(i+1);
+               }else if(list.get(i).data>key){
+                   succ = list.get(i);
+               }
+               flag=true;
+               break;
             }
-            else if(node.data > key){
-                if(list.get(1).data > node.data){
-                    list.set(1, node);
-                }
-            }
-            
-            if(node.left != null) q.offer(node.left);
-            if(node.right != null) q.offer(node.right);
         }
-
-        // when we couldn't find the answer
-        if(list.get(0).data == Integer.MIN_VALUE) list.set(0, null);
-        if(list.get(1).data == Integer.MAX_VALUE) list.set(1, null);
-        return list;
+        if(!flag && list.get(list.size()-1).data<key){
+            pre = list.get(list.size()-1);
+            
+        }else if(!flag){
+            succ = list.get(list.size()-1);
+        }
+       ArrayList<Node> result = new ArrayList<>();
+       result.add(pre);
+       result.add(succ);
+       return result;
+        
+        
+    }
+    public void inorder(Node node, List<Node> list){
+        if(node==null) return;
+        inorder(node.left,list);
+        list.add(node);
+        inorder(node.right,list);
     }
 }
