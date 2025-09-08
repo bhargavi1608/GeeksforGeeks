9c0b4e13b1e20fc84b1827c2f6c05c1013b96a6d
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-09-08 20:35:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Alternate Nodes/delete-alternate-nodes.java
@@ -0,0 +1,21 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution {
+    public void deleteAlt(Node head) {
+        // Code Here
+        Node curr = head;
+        while(curr!=null && curr.next!=null){
+            curr.next=curr.next.next;
+            curr=curr.next;
+        }
+       
+    }
+}
