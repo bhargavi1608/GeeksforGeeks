47f36235ffd689f28af90a17832d57e401859222
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-09-11 15:40:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Binary Tree to DLL/binary-tree-to-dll.java
@@ -0,0 +1,43 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node() {
+        this.data = 0;
+        this.left = this.right = null;
+    }
+
+    Node(int data) {
+        this.data = data;
+        this.left = this.right = null;
+    }
+}
+*/
+class Solution {
+    Node head=null;
+    Node tail=null;
+    
+    Node bToDLL(Node root) {
+        // code here
+        if(root==null) return null;
+       
+        inorder(root);
+        return head;
+    }
+    private void inorder(Node node){
+        if(node==null) return;
+        inorder(node.left);
+        if(tail==null){
+            head=node; // head contains of Node 25
+            
+        }else{
+            tail.right=node;
+            node.left=tail;
+        }
+        tail=node;
+        inorder(node.right);
+        
+        
+    }
+}
