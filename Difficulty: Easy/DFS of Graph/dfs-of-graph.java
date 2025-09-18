a517a269a53b7f167a5839790492651a69c6180a
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-09-18 14:22:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -0,0 +1,31 @@
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> res = new ArrayList<>();
+        int V=adj.size();
+        boolean[] visited = new boolean[V];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        while(!stack.isEmpty()){
+            int vertex = stack.pop();
+            if(!visited[vertex]){
+                visited[vertex]=true;
+                res.add(vertex);
+                //push the adjacency vertices in reverse order
+                List<Integer> n = adj.get(vertex);
+                for(int i=n.size()-1;i>=0;i--){
+                    int n1 = n.get(i);
+                    if(!visited[n1]){
+                        stack.push(n1);
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+        return res;
+        
+    }
+}
