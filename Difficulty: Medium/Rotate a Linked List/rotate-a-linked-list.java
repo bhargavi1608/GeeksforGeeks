84414a588e31dfaff562f7adc9ab66427e1cca78
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-09-24 11:36:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.java
+++ Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.java
@@ -11,32 +11,25 @@ class Node {
 */
 
 class Solution {
-    public int Find_length(Node head){
-        int length=0;
-        while(head!=null){
-            length++;
-            head=head.next;
-        }
-        return length;
-        
-    }
     public Node rotate(Node head, int k) {
         // code here
-        if(head==null || head.next==null) return head;
-        int length = Find_length(head);
-        k%=length;
-        if(k==0) return head;
+       if (head == null || head.next == null || k == 0) return head;
         Node temp=head;
+        int len=1;
         while(temp.next!=null){
             temp=temp.next;
+            len++;
         }
+        
+        k%=len;
+        if (k == 0) return head;
         temp.next=head;
-   
         Node newTail = head;
+       
         for(int i=1;i<k;i++){
             newTail=newTail.next;
         }
-        Node newHead=newTail.next;
+        Node newHead = newTail.next;
         newTail.next=null;
         return newHead;
     }
