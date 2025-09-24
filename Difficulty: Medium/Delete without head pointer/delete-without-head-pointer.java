884c5c9c2b588a229226af8bfe05422ab29b4d45
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-09-24 19:43:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Delete without head pointer/delete-without-head-pointer.java
+++ Difficulty: Medium/Delete without head pointer/delete-without-head-pointer.java
@@ -15,8 +15,7 @@ class Solution {
     public void deleteNode(Node del_node) {
         // code here
         if(del_node==null || del_node.next==null) return;
-        
-        del_node.data = del_node.next.data;
+        del_node.data=del_node.next.data;
         del_node.next=del_node.next.next;
     }
 }
