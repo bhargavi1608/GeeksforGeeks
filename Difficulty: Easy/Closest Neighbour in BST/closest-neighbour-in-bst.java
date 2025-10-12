45c2f6c97b0381ee414f4a4bc89f7b75e0db5faa
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-10-12 14:01:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Closest Neighbour in BST/closest-neighbour-in-bst.java
+++ Difficulty: Easy/Closest Neighbour in BST/closest-neighbour-in-bst.java
@@ -11,22 +11,18 @@ class Node {
 class Solution {
     public int findMaxFork(Node root, int k) {
         // code here.
-         if(root==null)return -1;
-        if(root.data==k)return k;
-        int left = -1;
-        if(root.left != null && root.data > k){
-            left = findMaxFork(root.left,k);
+        int res = -1;
+        while(root!=null){
+            if(root.data==k){
+                return root.data;
+            }
+            else if(root.data<k){
+                res = root.data;
+                root=root.right;
+            }else{
+                root=root.left;
+            }
         }
-        int right = -1;
-        if(root.right != null && root.data < k){
-            right = findMaxFork(root.right,k);
-        }
-        
-        if(root.data <k){
-            return Math.max(root.data,Math.max(left,right));
-        }
-        else{
-            return Math.max(left,right);
-        }
+        return res;
     }
 }
