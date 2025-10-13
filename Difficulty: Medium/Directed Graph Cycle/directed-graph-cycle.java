6f70577af9da52932ec57e73fdb01628555ac070
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-10-13 10:15:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,40 +1,47 @@
 class Solution {
     public boolean isCyclic(int V, int[][] edges) {
         // code here
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<V;i++){
-            adj.add(new ArrayList<>());
-            
-        }
-        // Indegree list
-        int[] indegree = new int[V];
-        for(int[] e:edges){
-            int u = e[0], v=e[1];
-            adj.get(u).add(v); //directed graph
-            indegree[v]++;
-            
-        }
-        //QUesue with Node of Indegree 0
-        Queue<Integer> q = new LinkedList<>();
-        
-        for(int i=0;i<V;i++){
-            if(indegree[i]==0){
-                q.offer(i);
-            }
-        }
-        int count=0; // to count the no.of processed nodes
-        while(!q.isEmpty()){
-           int node= q.poll();
+      ArrayList<List<Integer>> adj = new ArrayList<>();
+       for(int i=0;i<V;i++){
+           adj.add(new ArrayList<>());
+       }
+       for(int[] e:edges){
+           adj.get(e[0]).add(e[1]);
+           
+       }
+       
+       // compute the indegree
+       
+       int[] indegree = new int[V];
+       for(int u=0;u<V;u++){
+           for(int v:adj.get(u)){
+               indegree[v]++;
+           }
+       }
+       
+       //Initiasize the queue and insert whose indegreee is 0
+       Queue<Integer> q = new LinkedList<>();
+       for(int i=0;i<V;i++){
+           if(indegree[i]==0){
+               q.add(i);
+           }
+       }
+       
+       // BFS traversal
+       int count=0;
+       ArrayList<Integer> t = new ArrayList<>();
+       while(!q.isEmpty()){
+           int node = q.poll();
            count++;
-           for(int n:adj.get(node)){ //node=0-> 1
+           t.add(node); // t=[1,]
+           for(int n:adj.get(node)){
                indegree[n]--;
                if(indegree[n]==0){
-                   q.offer(n);
+                   q.add(n);
                }
            }
-        }
-        return count!=V;
-        
+       }
+       return count!=V;
         
     }
 }
