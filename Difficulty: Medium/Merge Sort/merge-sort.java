b0941e7d89237f387ebd98a885450776f1a53a79
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-12-23 11:29:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,83 +1,37 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] arr = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) arr[idx++] = i;
-
-            new Solution().mergeSort(arr, 0, arr.length - 1);
-
-            for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
-
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
 
     void mergeSort(int arr[], int l, int r) {
         // code here
-       if(l<r){
-           int m=(l+r)/2;
-           mergeSort(arr,l,m);
-           mergeSort(arr,m+1,r);
-           merge(arr,l,m,r);
-       }
+        if(l>=r) 
+            return;
+        int m = (l+r)/2;
+        mergeSort(arr,l,m);
+        mergeSort(arr,m+1,r);
+        merge(arr,l,m,r);
     }
-    public void merge(int[] arr,int l,int m,int r){
-       int n1 = m - l + 1;
-        int n2 = r - m;
-
-        // Temporary arrays
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-
-        // Copy data
-        for (int i = 0; i < n1; i++) left[i] = arr[l + i];
-        for (int j = 0; j < n2; j++) right[j] = arr[m + 1 + j];
-
-        int i = 0, j = 0, k = l;
-
-        // Merge the temp arrays back into arr[l..r]
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                arr[k++] = left[i++];
-            } else {
-                arr[k++] = right[j++];
+    void merge(int arr[],int l, int m, int r){
+        int[] temp = new int[r-l+1];
+        //temp array used to store the merged elements
+        int i=l,j=m+1, k=0;
+        while(i<=m && j<=r){
+            if(arr[i]<=arr[j]){
+                temp[k]=arr[i++];
+            }else{
+                temp[k]=arr[j++];
             }
+            k++;
         }
-
-        // Copy remaining elements
-        while (i < n1) {
-            arr[k++] = left[i++];
+        while(i<=m){
+            temp[k++]=arr[i++];
         }
-        while (j < n2) {
-            arr[k++] = right[j++];
+        while(j<=r){
+            temp[k++]=arr[j++];
+        }
+        //copy the sorted elements back to original array;
+        for(int x=0;x<temp.length;x++){
+            arr[l+x]=temp[x];
         }
         
     }
+   
 }
