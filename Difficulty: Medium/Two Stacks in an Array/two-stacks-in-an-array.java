3f1d0732f1853289e586afa340a5384866be1c37
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2025-12-27 15:28:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Two Stacks in an Array/two-stacks-in-an-array.java
+++ Difficulty: Medium/Two Stacks in an Array/two-stacks-in-an-array.java
@@ -2,41 +2,42 @@ class twoStacks {
     Stack<Integer> s1;
     Stack<Integer> s2;
     twoStacks() {
-    s1 = new Stack<>();
-    s2 = new Stack<>();
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+        
     }
+
     // Function to push an integer into the stack1.
     void push1(int x) {
         // code here
-        s1.push(x);
+       s1.push(x);
+        
     }
 
     // Function to push an integer into the stack2.
     void push2(int x) {
-        s2.push(x);
+        
         // code here
+        s2.push(x);
     }
 
     // Function to remove an element from top of the stack1.
         
     int pop1() {
-        // code heres2
-         if (s1.isEmpty()) {
-            return -1;
+        // code here
+        if(!s1.isEmpty()){
+            return s1.pop();
         }
-      int top =  s1.pop();
-      return top;
-        
+        return -1;
     }
 
         
     // Function to remove an element from top of the stack2.
     int pop2() {
         // code here
-         if (s2.isEmpty()) {
-            return -1;
+        if(!s2.isEmpty()){
+            return s2.pop();
         }
-        int top = s2.pop();
-        return top;
+        return -1;
     }
 }
