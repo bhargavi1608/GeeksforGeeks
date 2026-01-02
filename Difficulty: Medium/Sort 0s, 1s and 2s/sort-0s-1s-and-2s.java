f850c35bf06d87ce964cad5f423ebe37ab0d8a88
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-01-02 11:36:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,70 +1,29 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputArray = input.split("\\s+");
-            int a[] = new int[inputArray.length];
-
-            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
-
-            Solution ob = new Solution();
-            ob.sort012(a);
-
-            for (int num : a) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-
-// } Driver Code Ends
-
 class Solution {
-    // Function to sort an array of 0s, 1s, and 2s
     public void sort012(int[] arr) {
         // code here
-        int n = arr.length;
         int low =0;
-        int high = n-1;
-        int mid= 0;
+        int mid =0;
+        int high = arr.length-1;
+        
         while(mid<=high){
             if(arr[mid]==0){
-                swap(arr,low,mid);
+                int temp = arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
                 low++;
                 mid++;
             }
             else if(arr[mid]==1){
-                mid+=1;
-            }
-            else{
-                swap(arr,mid,high);
+                mid++;
+            }else{
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high]=temp;
                 high--;
+               
             }
+          
             
         }
-        
     }
-        public static void swap(int[] arr, int i, int j){
-            
-            int temp = arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-        }
-        
-    
 }
-
-
-//{ Driver Code Starts.
-// } Driver Code Ends
