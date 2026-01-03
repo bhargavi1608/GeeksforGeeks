113f27e24c54ca899a81e8e1fd7af33ff2844c84
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-01-03 14:22:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int findPages(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        if(k>n) return -1;
+        int low=0,high=0;
+        for(int pages:arr){
+            low=Math.max(low,pages);
+            high+=pages;
+        }
+        int ans =-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(isPossible(arr,k,mid)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public boolean isPossible(int[] arr, int k, int mid){
+        int student =1;
+        int currSum = 0;
+        for(int pages:arr){
+            if(currSum+ pages >mid){
+                student++;
+                currSum=pages;
+                if(student>k){
+                    return false;
+                }
+            }
+            else{
+                currSum+=pages;
+            }
+          
+        }
+        return true;
+    }
+   
+}
