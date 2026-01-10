c79c88020f6320b5244827c8fb57aceaa3a93bd3
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-01-10 10:23:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
@@ -1,30 +1,23 @@
 class Solution {
     public int countSubstr(String s, int k) {
-        return helper(s, k) - helper(s, k - 1);
+        //  code here
+        return substr(s,k)-substr(s,k-1);
     }
-
-    private int helper(String s, int k) {
-        int left = 0, right = 0, count = 0, distinctCount = 0;
-        int[] freq = new int[256]; // Array for character frequency (assuming ASCII)
-
-        while (right < s.length()) {
-            if (freq[s.charAt(right)] == 0) {
-                distinctCount++; // New distinct character
-            }
-            freq[s.charAt(right)]++;
-
-            while (distinctCount > k) {
-                freq[s.charAt(left)]--;
-                if (freq[s.charAt(left)] == 0) {
-                    distinctCount--; // Remove distinct character
+    public int substr(String s,int k){
+        HashMap<Character,Integer> map = new HashMap<>();
+        int left=0;
+        int count=0;
+        for(int right=0;right<s.length();right++){
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+            while(map.size()>k){
+                map.put(s.charAt(left),map.get(s.charAt(left))-1);
+                if(map.get(s.charAt(left))==0){
+                    map.remove(s.charAt(left));
                 }
                 left++;
             }
-
-            count += right - left + 1;
-            right++;
+            count+=(right-left)+1;
         }
         return count;
     }
 }
-
