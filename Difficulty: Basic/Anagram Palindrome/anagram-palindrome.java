00eda69860b41f6a92d9bb18f7db91c1786381ad
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-02-12 09:53:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Anagram Palindrome/anagram-palindrome.java
+++ Difficulty: Basic/Anagram Palindrome/anagram-palindrome.java
@@ -1,18 +1,28 @@
 class Sol {
     int isPossible(String S) {
         // your code here
-       Map<Character,Integer> map = new HashMap<>();
-       for(char c:S.toCharArray()){
-           map.put(c,map.getOrDefault(c,0)+1);
-       }
-       int oddCount=0;
-       for(int value:map.values()){
-           if(value%2!=0) oddCount++;
-       }
-       if(S.length()%2==0 && oddCount==0)  return 1;
-       if(S.length()%2!=0 && oddCount==1) return 1;
-       return 0;
-       
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c:S.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
         
+        int f1 = 0;
+        int odf =0;
+        for(Map.Entry<Character,Integer> e: map.entrySet()){
+            if(e.getValue() ==1){
+                f1++;
+                
+            }
+            if(e.getValue()%2!=0){
+                odf++;
+            }
+            
+            
+        }
+        
+        if(f1>1 || odf >1){
+            return 0;
+        }
+        return 1;
     }
 }
