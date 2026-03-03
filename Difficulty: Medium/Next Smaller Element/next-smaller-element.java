cc639c63227eeee64a8314641c0ad05a52478006
Pulloju Bhargavi <155425753+bhargavi1608@users.noreply.github.com>
2026-03-03 11:39:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Smaller Element/next-smaller-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    static ArrayList<Integer> nextSmallerEle(int[] arr) {
+        // code here
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> s = new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
+            while(!s.isEmpty() && s.peek()>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                res.add(-1);
+            }else{
+                res.add(s.peek());
+            }
+           s.push(arr[i]);
+            
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
